7ef6a0636b97a7eec15836880db85208ff368669
hannalee809 <hlee@smith.edu>
2022-02-20 12:42:46 -0500

    added tests for shiftLetter, encipher, and decipher functions

--- test_cipher2.py
+++ test_cipher2.py
@@ -1,13 +1,29 @@
+import pytest
 from cipher2 import *
 
-def test_shiftLetter():
-    assert shiftLetter("A", 2) == "C"
-    assert shiftLetter("!", 2) == "!"
+@pytest.mark.parametrize("input_x, expected", 
+       [pytest.param("A", "C", id = "uppercase_letter"), 
+       pytest.param("!", "!", id = "not_letter"), 
+       pytest.param("a", "c", id = "lowercase_letter"),
+       pytest.param("z", "b", id = "returns_outside_lowercase_range"),
+       pytest.param("Z", "B", id = "returns_outside_uppercase_range")
+       ])
 
-# class test_shiftLetter:
-#     def test01(self):
-#         assert shiftLetter(char = "A", shift = 2) == "C"
+def test_shiftLetter(input_x, expected):
+    assert shiftLetter(input_x, 2) == expected
 
-#     def test02(self):
-#         assert shiftLetter(char = "!", shift = 2) == "!"
-       
+@pytest.mark.parametrize("message_decrypted, message_encrypted",
+        [pytest.param("abcdef", "hjlnpr", id = "all_lowercase"),
+        pytest.param("aBcDef", "hJlNpr", id = "mixed_cases"), 
+        pytest.param("ab c!e", "hj l!o", id = "spaces_and_symbols")])
+
+def test_encipherShift(message_decrypted, message_encrypted):
+    assert encipherShift(message_decrypted, "HIJKLM") == message_encrypted
+
+@pytest.mark.parametrize("message_decrypted, message_encrypted",
+        [pytest.param("abcdef", "hjlnpr", id = "all_lowercase"),
+        pytest.param("aBcDef", "hJlNpr", id = "mixed_cases"), 
+        pytest.param("ab c!e", "hj l!o", id = "spaces_and_symbols")])
+
+def test_decipherShift(message_encrypted, message_decrypted):
+    assert decipherShift(message_encrypted, "HIJKLM") == message_decrypted
